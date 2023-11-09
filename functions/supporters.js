d6cc6a5480b576516660c04ddcde2bd6127266de
Damien O'Sullivan <damosullivan@hubspot.com>
2023-11-09 12:16:30 +0000

    Take 3. Will set up wrangler after this

--- functions/supporters.js
+++ functions/supporters.js
@@ -1,12 +1,15 @@
-export function onRequestGet(context) {
-  return new Request("https://developers.buymeacoffee.com/api/v1/supporters", {
+export async function onRequestGet(context) {
+  const supporters = await new Request("https://developers.buymeacoffee.com/api/v1/supporters", {
     headers: {
       Authorization: `Bearer ${context.env.COFFEE_API_KEY}`,
     },
   })
     .then((res) => res.json())
-    .then((json) => json.data)
-    .sort((a, b) => {
+    .then((json) => json.data);
+
+    console.log(supporters);
+
+    return supporters.sort((a, b) => {
       return a.support_coffees - b.support_coffees;
     })
     .map((d) => {
