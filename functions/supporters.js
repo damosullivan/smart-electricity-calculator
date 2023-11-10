aac8c505bedf08e24e8936e40aa49907a42556f9
Damien O'Sullivan <damosullivan@hubspot.com>
2023-11-10 11:27:01 +0000

    Paginated

--- functions/supporters.js
+++ functions/supporters.js
@@ -1,22 +1,29 @@
+async function fetchPage(context, page) {
+  const response = await fetch(
+    `https://developers.buymeacoffee.com/api/v1/supporters?page=${page}`,
+    {
+      headers: {
+        Authorization: `Bearer ${context.env.COFFEE_API_KEY}`,
+      },
+    }
+  );
+  return await response.json();
+}
+
 export async function onRequestGet(context) {
+  const page = 1;
+  const supporter_names = [];
 
-  const response = await fetch("https://developers.buymeacoffee.com/api/v1/supporters", {
+  do {
+    const response = fetchPage(context, page);
+    supporter_names.push(...response.data.map((s) => s.supporter_name));
+    page++;
+  } while (response.next_page);
+
+  return new Response(JSON.stringify(supporter_names), {
     headers: {
-      Authorization: `Bearer ${context.env.COFFEE_API_KEY}`,
+      // we'll set a CORS header to allow access to this resource from everywhere
+      "Access-Control-Allow-Origin": "*",
     },
-  })
-
-	const result = await response.json()
-  const supporter_names = result.data.map(s => s.supporter_name);
-
-  return new Response(
-		JSON.stringify(supporter_names),
-		{
-			headers: {
-				// we'll set a CORS header to allow access to this resource from everywhere
-				'Access-Control-Allow-Origin': '*',
-			},
-		}
-	)}
-
-
+  });
+}
