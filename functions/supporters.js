a11be2de7c68edcb4c42b5fafd672e687e70bdf0
Damien O'Sullivan <damosullivan@hubspot.com>
2023-11-10 10:38:59 +0000

    Attempting Supporters once again

--- functions/supporters.js
+++ functions/supporters.js
@@ -1,18 +1,55 @@
-export async function onRequestGet(context) {
-  const supporters = await new Request("https://developers.buymeacoffee.com/api/v1/supporters", {
+// export async function onRequestGet(context) {
+//   const supporters = await new Request("https://developers.buymeacoffee.com/api/v1/supporters", {
+//     headers: {
+//       Authorization: `Bearer ${context.env.COFFEE_API_KEY}`,
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((json) => json.data);
+
+//     console.log(supporters);
+
+//     return supporters.sort((a, b) => {
+//       return a.support_coffees - b.support_coffees;
+//     })
+//     .map((d) => {
+//       d.supporter_name;
+//     });
+// }
+
+
+// 1. listen for fetch events
+addEventListener('fetch', (event) => {
+	event.respondWith(handleRequest(event.request))
+})
+
+// 2. event handler which returns a Response object
+async function handleRequest(event) {
+	const url = `http://ws.audioscrobbler.com/2.0/?format=json&method=user.getrecenttracks&user=your-username&limit=1&api_key=${LASTFM_API_KEY}`
+
+	// we'll use `fetch` in combination with `await` so we don't have to manually resolve the returned `Promise`
+	// this is why we defined the whole function as `async`, so we can use `await`
+	const response = await fetch("https://developers.buymeacoffee.com/api/v1/supporters", {
     headers: {
       Authorization: `Bearer ${context.env.COFFEE_API_KEY}`,
     },
   })
-    .then((res) => res.json())
-    .then((json) => json.data);
 
-    console.log(supporters);
+	// `fetch` will resolve to a [Response object](https://developer.mozilla.org/en-US/docs/Web/API/Response)
+	// We will use the `json` method to return the responses results as a JavaScript object
+	// note that we'll again use `await` since .json() returns a `Promise`
 
-    return supporters.sort((a, b) => {
-      return a.support_coffees - b.support_coffees;
-    })
-    .map((d) => {
-      d.supporter_name;
-    });
+	const result = await response.json()
+
+  return new Response(
+		// we'll use JSON.stringify() to convert the returned JavaScript object to a string which can be sent in a response
+		JSON.stringify(response),
+		{
+			headers: {
+				// we'll set a CORS header to allow access to this resource from everywhere
+				'Access-Control-Allow-Origin': '*',
+			},
+		}
+	)
+
 }
